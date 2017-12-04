fbdd6899b757ab9495f29cc594411cb4cb098c35
shopglobal <president@worldvaporexpo.com>
2017-12-04 03:21:56 -0500

    safely storing the flytcoin and volleycoin blockchains

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{                                                           
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x14,0x3d, 0x8, 0xe3, 0x49, 0x4f,0x8, 0x19, 0x2b, 0x2b, 0x8, 0xe3, 0x31, 0x31, 0x8, 0x19 } };
+}
